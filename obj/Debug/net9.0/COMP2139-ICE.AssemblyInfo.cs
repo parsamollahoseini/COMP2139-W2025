33d1dcf05d76be8ccd9ea1b2cbccacb6afc3e824
parsa molahosseini <parsamollahoseini7@gmail.com>
2025-01-18 14:43:15 -0500

    new update for part 1

--- obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
+++ obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e83f86e53e1d53c843d2ff62f15dea54aa3c1df")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
